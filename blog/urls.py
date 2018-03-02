f2fb9486abc40cc4730c3bf0e970828174d4a639
Greg Griffin <griffin.ge@gmail.com>
2018-03-01 19:41:43 -0800

    extended base template, added post_detail template

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Only an empty string will match; tells django that
     # views.post_list is place to go at blank url
     path('', views.post_list, name='post_list'),
+    path('post/(<int:pk>/', views.post_detail, name='post_detail'),
 ]
